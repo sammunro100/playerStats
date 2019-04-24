a866359484dba65d9b62b26c496e1e3b83b72930
Sam Munro <sam.munro@cgi.com>
2019-04-24 09:58:31 +0100

    Initial Commit

---
+++ playerStats/README.md
@@ -0,0 +1,3 @@
+"# playerStats" 
+Use Brackets, or any other IDE that supports the use of a live enviroment running index.html
+I used VS code and used the Live Server plugin, right click the index.html and open with live server.
---
+++ playerStats/app.js
@@ -0,0 +1,436 @@
+
+
+// I could have plugged the data right into and variable but seemed best to emulate an api call to some extent.
+function getPlayerData() {
+  return {
+    "players": [
+      {
+        "player": {
+          "info": {
+            "position": "D",
+            "shirtNum": 4,
+            "positionInfo": "Centre/Right Central Defender"
+          },
+          "nationalTeam": {
+            "isoCode": "BE",
+            "country": "Belgium",
+            "demonym": "Belgian"
+          },
+          "age": "27 years 139 days",
+          "name": {
+            "first": "Toby",
+            "last": "Alderweireld"
+          },
+          "id": 4916,
+          "currentTeam": {
+            "name": "Tottenham Hotspur",
+            "teamType": "FIRST",
+            "shortName": "Spurs",
+            "id": 21
+          }
+        },
+        "stats": [
+          {
+            "name": "goals",
+            "value": 5
+          },
+          {
+            "name": "losses",
+            "value": 20
+          },
+          {
+            "name": "wins",
+            "value": 48
+          },
+          {
+            "name": "draws",
+            "value": 23
+          },
+          {
+            "name": "fwd_pass",
+            "value": 1533
+          },
+          {
+            "name": "goal_assist",
+            "value": 2
+          },
+          {
+            "name": "appearances",
+            "value": 80
+          },
+          {
+            "name": "mins_played",
+            "value": 6953
+          },
+          {
+            "name": "backward_pass",
+            "value": 308
+          }
+        ]
+      },
+      {
+        "player": {
+          "info": {
+            "position": "M",
+            "shirtNum": 42,
+            "positionInfo": "Centre Defensive Midfielder"
+          },
+          "nationalTeam": {
+            "isoCode": "CI",
+            "country": "Cote D'Ivoire",
+            "demonym": "Ivorian"
+          },
+          "age": "33 years 67 days",
+          "name": {
+            "first": "Yaya",
+            "last": "Touré"
+          },
+          "id": 4148,
+          "currentTeam": {
+            "name": "Manchester City",
+            "teamType": "FIRST",
+            "shortName": "Man City",
+            "id": 11
+          }
+        },
+        "stats": [
+          {
+            "name": "goals",
+            "value": 65
+          },
+          {
+            "name": "losses",
+            "value": 49
+          },
+          {
+            "name": "wins",
+            "value": 149
+          },
+          {
+            "name": "draws",
+            "value": 35
+          },
+          {
+            "name": "fwd_pass",
+            "value": 4491
+          },
+          {
+            "name": "goal_assist",
+            "value": 35
+          },
+          {
+            "name": "appearances",
+            "value": 232
+          },
+          {
+            "name": "mins_played",
+            "value": 18919
+          },
+          {
+            "name": "backward_pass",
+            "value": 1995
+          }
+        ]
+      },
+      {
+        "player": {
+          "info": {
+            "position": "F",
+            "shirtNum": 10,
+            "positionInfo": "Left/Centre/Right Second Striker"
+          },
+          "nationalTeam": {
+            "isoCode": "GB-ENG",
+            "country": "England",
+            "demonym": "English"
+          },
+          "age": "30 years 269 days",
+          "name": {
+            "first": "Wayne",
+            "last": "Rooney"
+          },
+          "id": 2064,
+          "currentTeam": {
+            "name": "Manchester United",
+            "teamType": "FIRST",
+            "shortName": "Man Utd",
+            "id": 12
+          }
+        },
+        "stats": [
+          {
+            "name": "goals",
+            "value": 201
+          },
+          {
+            "name": "losses",
+            "value": 91
+          },
+          {
+            "name": "wins",
+            "value": 280
+          },
+          {
+            "name": "draws",
+            "value": 90
+          },
+          {
+            "name": "fwd_pass",
+            "value": 1795
+          },
+          {
+            "name": "goal_assist",
+            "value": 84
+          },
+          {
+            "name": "appearances",
+            "value": 461
+          },
+          {
+            "name": "mins_played",
+            "value": 27056
+          },
+          {
+            "name": "backward_pass",
+            "value": 1928
+          }
+        ]
+      },
+      {
+        "player": {
+          "info": {
+            "position": "D",
+            "shirtNum": 4,
+            "positionInfo": "Centre Central Defender"
+          },
+          "nationalTeam": {
+            "isoCode": "DE",
+            "country": "Germany",
+            "demonym": "German"
+          },
+          "age": "31 years 294 days",
+          "name": {
+            "first": "Per",
+            "last": "Mertesacker"
+          },
+          "id": 4246,
+          "currentTeam": {
+            "name": "Arsenal",
+            "teamType": "FIRST",
+            "shortName": "Arsenal",
+            "id": 1
+          }
+        },
+        "stats": [
+          {
+            "name": "goals",
+            "value": 8
+          },
+          {
+            "name": "losses",
+            "value": 45
+          },
+          {
+            "name": "wins",
+            "value": 117
+          },
+          {
+            "name": "draws",
+            "value": 41
+          },
+          {
+            "name": "fwd_pass",
+            "value": 4257
+          },
+          {
+            "name": "appearances",
+            "value": 187
+          },
+          {
+            "name": "mins_played",
+            "value": 16531
+          },
+          {
+            "name": "backward_pass",
+            "value": 535
+          }
+        ]
+      },
+      {
+        "player": {
+          "info": {
+            "position": "M",
+            "shirtNum": 26,
+            "positionInfo": "Left/Right Winger"
+          },
+          "nationalTeam": {
+            "isoCode": "DZ",
+            "country": "Algeria",
+            "demonym": "Algerian"
+          },
+          "age": "25 years 149 days",
+          "name": {
+            "first": "Riyad",
+            "last": "Mahrez"
+          },
+          "id": 8983,
+          "currentTeam": {
+            "name": "Leicester City",
+            "teamType": "FIRST",
+            "shortName": "Leicester",
+            "id": 26
+          }
+        },
+        "stats": [
+          {
+            "name": "goals",
+            "value": 22
+          },
+          {
+            "name": "losses",
+            "value": 23
+          },
+          {
+            "name": "wins",
+            "value": 35
+          },
+          {
+            "name": "draws",
+            "value": 21
+          },
+          {
+            "name": "fwd_pass",
+            "value": 687
+          },
+          {
+            "name": "goal_assist",
+            "value": 14
+          },
+          {
+            "name": "appearances",
+            "value": 71
+          },
+          {
+            "name": "mins_played",
+            "value": 5368
+          },
+          {
+            "name": "backward_pass",
+            "value": 323
+          }
+        ]
+      }
+    ]
+  }
+}
+
+/** Honestly I imagine that having all the variables out in the global name space like this isn't best practice in this format, 
+ * it's more coming from an Angular background and a testing perspective having variables nested within functions makes testing there values
+ * particulary difficult if at all possible. I could prehaps wrap all of these values into an object to help prevent name space collision.
+*/
+
+let playerData;
+let playerImgString;
+let clubSpriteImgString;
+let playerPictureEle = document.getElementById('playerImage');
+let playerNameEle = document.getElementById('playerName');
+let playerPosEle = document.getElementById('playerRole');
+let playerClubEle = document.getElementById('playerClubSprite');
+let selectEle = document.getElementById('playerNamesList');
+let bottomHalf = document.getElementById('bottomHalf');
+let optionEle = document.getElementById('playerNamesList');
+let currentFootaballerId;
+let currentFootballer;
+let appearancesEle = document.getElementById('appearances');
+let goalsEle = document.getElementById('goals');
+let assistEle = document.getElementById('assists');
+let gpmEle = document.getElementById('GPM');
+let ppmEle = document.getElementById('PPM');
+let gpm;
+let ppm;
+let goals;
+let passesF;
+let passesB;
+let appearances;
+let minutesPlayed;
+
+(function () {
+  playerData = getPlayerData();
+  setHtmlValues(playerData.players[0].player);
+  setStatElements(playerData.players[0].stats)
+  createOptElements();
+}())
+
+function getPlayerPosition(playerPositionChar) {
+  switch (playerPositionChar) {
+    case 'M':
+      return 'Midfielder'
+    case 'F':
+      return 'Striker'
+    case 'D':
+      return 'Defender'
+    default:
+  }
+}
+
+function setHtmlValues(playerElement) {
+  playerImgString = `./assets/footballPlayers/${playerElement.id}.png`
+  clubSpriteImgString = `./assets/footballClubSprites/${playerElement.currentTeam.id}.png`
+  playerClubEle.setAttribute('src', clubSpriteImgString);
+  playerPictureEle.setAttribute('src', playerImgString);
+  playerNameEle.innerHTML = playerElement.name.first + ' ' + playerElement.name.last;
+  playerPosEle.innerHTML = getPlayerPosition(playerElement.info.position);
+}
+
+function createOptElements() {
+  playerData.players.forEach((ele) => {
+    let playerName = ele.player.name.first + ' ' + ele.player.name.last;
+    let newOptionElement = document.createElement('option');
+    newOptionElement.setAttribute("value", ele.player.id);
+    newOptionElement.innerHTML = playerName;
+    selectEle = document.getElementById("playerNamesList");
+    selectEle.appendChild(newOptionElement);
+  })
+}
+
+function getOptValue() {
+  currentFootaballerId = optionEle.options[optionEle.selectedIndex].value;
+  currentFootballer = playerData.players.find(ele => ele.player.id.toString() === currentFootaballerId);
+  setHtmlValues(currentFootballer.player)
+  setStatElements(currentFootballer.stats);
+}
+
+function setStatElements(playerStats) {
+  playerStats.forEach((stat) => {
+    getStatTitle(stat);
+  })
+  gpmEle.innerHTML = Math.round((goals / appearances) * 100) / 100;
+  ppmEle.innerHTML = Math.round((passesB + passesF) / (minutesPlayed) * 100) / 100;
+}
+
+function getStatTitle(stat) {
+  switch (stat.name) {
+    case 'goals':
+      goalsEle.innerHTML = stat.value;
+      goals = stat.value;
+      break;
+    case 'fwd_pass':
+      passesF = stat.value;
+      break;
+    case 'backward_pass':
+      passesB = stat.value;
+      break;
+    case 'goal_assist':
+      assistEle.innerHTML = stat.value;
+      break;
+    case 'appearances':
+      appearancesEle.innerHTML = stat.value;
+      appearances = stat.value;
+      break;
+    case 'mins_played':
+      minutesPlayed = stat.value;
+      break;
+  }
+}
+
+
